a8f726b70a9eb4f7fa3fe6fbc9ea79d8a348fca7
Jair De Jesus Uribe Valencia <xdreyveringx@hotmail.com>
2023-12-15 01:52:52 -0500

    fit

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,29 +1,38 @@
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-import { Link } from "react-router-dom";
 
-const MenuItem = ({ menu, index }) => {
+const MenuItem = ({ menu }) => {
+  const handleClick = () => {
+    const section = document.getElementById(menu.uri.substring(1)); // Quitamos el "#" del URI
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  };
+
   const [isHoverred, setIsHoverred] = useState(false);
+
   return (
-    <Link to={menu.uri} className="w-12 h-12 rounded-full flex items-center justify-center group cursor-pointer hover:bg-gradient-to-br hover:from-primary hover:to-secondary relative"
-    onMouseEnter={() => setIsHoverred(true)} 
-    onMouseLeave={() => setIsHoverred(false)}>
-
-      <menu.Icon className={`text-texlight group-hover:text-bgPrimary text-xl`} />
-
+    <div
+      className="w-12 h-12 rounded-full flex items-center justify-center group cursor-pointer hover:bg-gradient-to-br hover:from-primary hover:to-secondary relative"
+      onMouseEnter={() => setIsHoverred(true)}
+      onMouseLeave={() => setIsHoverred(false)}
+      onClick={handleClick}
+    >
+      <menu.Icon className={`text-texlight text-xl`} />
       <AnimatePresence>
         {isHoverred && (
           <motion.div
-          initial={{ opacity: 0, x:-25 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x:-25 }} 
-          className="hidden lg:block absolute bg-white rounded-md px-6 py-2 -left-[190px] after:absolute after:-right-1 after:top-3 after:w-3 after:h-3 after:bg-white after:rotate-45"
-          style={{boxShadow : "inset 0px 0px 10px rgba(0, 0, 0, 0.4)"}}>
-          <p className="text-bgPrimary">{menu.name}</p>
+            initial={{ opacity: 0, x: -25 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: -25 }}
+            className="hidden lg:block absolute bg-white rounded-md px-6 py-2 -left-[190px] after:absolute after:-right-1 after:top-3 after:w-3 after:h-3 after:bg-white after:rotate-45"
+            style={{ boxShadow: "inset 0px 0px 10px rgba(0, 0, 0, 0.4)" }}
+          >
+            <p className="text-bgPrimary">{menu.name}</p>
           </motion.div>
         )}
       </AnimatePresence>
-    </Link>
+    </div>
   );
 };
 
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import About from "../containers/About";
 import Contact from "../containers/Contact";
 import Header from "../containers/Header";
@@ -11,27 +10,16 @@ import Home from "./Home";
 
 const App = () => {
   return (
-    <Router>
       <div className="w-full xl:w-[1600px] py-32 px-4 lg:px-12 pr-4 lg:pr-32">
         <ParticlesContainer />
-        < Home />
-        < Header />
+        <Home />
+        <Header />
         <About />
         <Skills />
-        < Projects />
+        <Projects />
         <Contact />
-        
-
-        <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
         <Footer />
       </div>
-    </Router>
   );
 };
 
